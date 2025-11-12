f6bb4041b44c456a1d6ee8e7c0b0cfeb9adabee4
SomashekarMH <66898227+SomashekarMH@users.noreply.github.com>
2025-11-12 09:36:25 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! Welcome Somu for CloudDevOps Team and do your best do hard work");
+        System.out.println("Hello, World! Welcome Somu for CloudDevOps Team and do your best do hard work and get the good job");
     }
 }
